0699d5bb923e7d1c059342e010207c9c1eb4d305
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-10-27 18:51:41 +0300

    bugFixed
    Deleted NotFoundPage and made a redirect to home  for any other pages except home, catalog and favorites

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,8 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import Home from 'pages/Home';
 import Catalog from 'pages/Catalog';
 import Favorites from 'pages/Favorites';
-import NotFound from 'pages/NotFound';
 
 export const App = () => {
   return (
@@ -14,7 +13,7 @@ export const App = () => {
           <Route path="/catalog" element={<Catalog />} />
           <Route path="/favorites" element={<Favorites />} />
         </Route>
-        <Route path="*" element={<NotFound />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </>
   );
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,11 +1,9 @@
 import CarList from 'components/CarList/CarList';
-// import CatalogSelects from 'components/Select/CatalogSelects';
 import { UptoTopBtn } from 'components/UptoTopBtn/UptoTopBtn';
 
 const Catalog = () => {
   return (
     <>
-      {/* <CatalogSelects /> */}
       <CarList />
       <UptoTopBtn />
     </>
